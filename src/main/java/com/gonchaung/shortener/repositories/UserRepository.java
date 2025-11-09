52f9661a0374c23067b65ea9d5907935fd96ac23
Pandas-12 <thantsithu.naing@icloud.com>
2025-11-09 21:27:52 +0700

    first commit

---
+++ src/main/java/com/gonchaung/shortener/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gonchaung.shortener.repositories;
+
+import com.gonchaung.shortener.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
